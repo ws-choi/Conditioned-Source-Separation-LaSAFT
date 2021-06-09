9d98238d5be750947464c9e221e24aa84396f7e0
Woosung Choi <ws_choi@korea.ac.kr>
2021-06-09 13:02:44 +0900

    gh-pages

--- dl_musdb18_samples.py
+++
@@ -1,4 +0,0 @@
-import musdb
-from lasaft.utils.functions import mkdir_if_not_exists
-mkdir_if_not_exists('etc')
-musdb.DB(root='etc/musdb18_dev', download=True)
--- lasaft/pretrained/__init__.py
+++
@@ -1,1 +0,0 @@
-from .load_pretrained_nets import PreTrainedLaSAFTNet
