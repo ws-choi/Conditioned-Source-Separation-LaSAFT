25433f2d6a6fce33c0251e117a6a768d6418acca
Woosung Choi <ws_choi@korea.ac.kr>
2021-08-12 16:12:53 +0900

    overlap-aware evaluation

--- eval.py
+++ eval.py
@@ -1,14 +1,9 @@
-from argparse import ArgumentParser
-from datetime import datetime
-
 import dotenv
 import hydra
 from omegaconf import DictConfig, OmegaConf
-
 from pytorch_lightning.utilities.distributed import rank_zero_info
 
-from lasaft.source_separation.conditioned.scripts import evaluator as evaluator
-from lasaft.utils.functions import mkdir_if_not_exists
+from lasaft.source_separation.conditioned.scripts import evaluator
 
 dotenv.load_dotenv(override=True)
 
